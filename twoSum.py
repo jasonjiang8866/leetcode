7e1226ae278dd0bc3c7dc82f8ff15f0ea3ac5b30
JYC <36893905+jasonjiang8866@users.noreply.github.com>
2019-05-20 02:15:59 +0800

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashTable={}
+        for index, item in enumerate(nums):
+            compliment=target-item
+            if hashTable.get(compliment, None) is not None:
+                return [hashTable[compliment], index]
+            else:
+                hashTable[item]=index
